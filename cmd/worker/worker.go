7cf5972901e36d889e682885d775fa210208ae1e
Tom Wieczorek <twieczorek@mirantis.com>
2022-09-05 16:31:46 +0200

    WIP
    Signed-off-by: Tom Wieczorek <twieczorek@mirantis.com>

--- cmd/worker/worker.go
+++ cmd/worker/worker.go
@@ -148,6 +148,7 @@ func (c *Command) Start(ctx context.Context) error {
 
 		reconciler := nllb.NewReconciler(&c.K0sVars, sp, envoyProxyImage, pullPolicy)
 		kubeletKubeconfig = reconciler.GetKubeletKubeconfig()
+		staticPods = sp
 
 		componentManager.Add(ctx, sp)
 		componentManager.Add(ctx, reconciler)
