1a9717b7dba5a895afa55b7e14228269d0d2155c
Alexey Makhov <amakhov@mirantis.com>
2024-06-27 10:18:06 +0300

    Upload airgap image list as release artifact. Add envoy image to the airgap image list.
    Signed-off-by: Alexey Makhov <amakhov@mirantis.com>

--- pkg/airgap/images.go
+++ pkg/airgap/images.go
@@ -40,6 +40,7 @@ func GetImageURIs(spec *v1beta1.ClusterSpec, all bool) []string {
 		spec.Images.KubeRouter.CNI.URI(),
 		spec.Images.KubeRouter.CNIInstaller.URI(),
 		spec.Images.MetricsServer.URI(),
+		spec.Network.NodeLocalLoadBalancing.EnvoyProxy.Image.URI(),
 		pauseImage.URI(),
 	}
 
